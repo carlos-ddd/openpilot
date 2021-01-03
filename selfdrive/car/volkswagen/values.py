d9be09e33111146d783c34aee0559fe119818263
Karl Dietmann <karl-dietmann@web.de>
2021-01-03 19:18:05 +0100

    driver input torque for wheel grabbed 80 -> 60 for better detection

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -19,7 +19,7 @@ class CarControllerParams:
   STEER_MAX = 300                # Max heading control assist torque 3.00 Nm
   STEER_DELTA_UP = 10            # Max HCA reached in 0.60s (STEER_MAX / (50Hz * 0.60))
   STEER_DELTA_DOWN = 10          # Min HCA reached in 0.60s (STEER_MAX / (50Hz * 0.60))
-  STEER_DRIVER_ALLOWANCE = 80
+  STEER_DRIVER_ALLOWANCE = 60
   STEER_DRIVER_MULTIPLIER = 3    # weight driver torque heavily
   STEER_DRIVER_FACTOR = 1        # from dbc
 
